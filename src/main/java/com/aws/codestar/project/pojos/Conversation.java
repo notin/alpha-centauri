5e2cf7b0cce0a1c03353ee0708993ad5ad98eda6
chadforgit <chadforgit@gmail.com>
2018-12-16 10:45:55 -0800

    asdf

--- src/main/java/com/aws/codestar/project/pojos/Conversation.java
+++ src/main/java/com/aws/codestar/project/pojos/Conversation.java
@@ -1,23 +1,15 @@
 
 package com.aws.codestar.project.pojos;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.aws.codestar.project.util.Helper;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.List;
 
 @Data
 @Builder
@@ -30,18 +22,6 @@ public class Conversation {
     public Integer id;
     @JsonProperty("messages")
     public List<Message> messages = null;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
     @Override
     public String toString()
